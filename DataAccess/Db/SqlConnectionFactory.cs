e3be1f5a4d3d016b6a5a21d6bd6c1cbca1cda13c
andres <andres.pena@kpginc.com>
2026-06-04 20:33:24 -0500

    andres-actualizaciones_1_0 : Se realiza la fabrica de conexiones hacia la base de datos.

---
+++ DataAccess/Db/SqlConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess.Db
+{
+    public class SqlConnectionFactory : IDbConnectionFactory
+    {
+
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
